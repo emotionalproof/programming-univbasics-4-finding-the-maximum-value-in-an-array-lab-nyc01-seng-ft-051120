dac5fcbc5092c2edb4cab01ab7cb81dd9f8d476b
Chase <chaserabenn@gmail.com>
2020-05-05 02:11:35 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -5,3 +5,13 @@
 def find_max_value(array)
   array.max
 end
+
+def find_max_value(array)
+  count = 0
+  max_value = -1
+  while count < array.length
+    max_value = array[count] if max_value < array[count]
+    count += 1
+  end
+  max_value
+end
